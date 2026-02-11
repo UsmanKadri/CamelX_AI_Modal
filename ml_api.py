8652eef2c2b0df43e20c0d109bc1a3e8a0bf040e
UsmanKadri <usman.kadri@live.com>
2026-02-11 22:39:57 +0530

    Add AI model and FastAPI service

---
+++ ml_api.py
@@ -0,0 +1,39 @@
+from fastapi import FastAPI
+import joblib
+import numpy as np
+
+app = FastAPI()
+
+model = joblib.load("Camel_Risk_LightGBM.pkl")
+
+@app.get("/")
+def home():
+    return {"status": "CamelX AI Running"}
+
+@app.post("/predict")
+def predict(data: dict):
+
+    features = np.array([[
+        data["heart_rate"],
+        data["spo2"],
+        data["temperature"],
+        data["bp_sys"],
+        data["bp_dia"],
+        data["battery"],
+    ]])
+
+    result = model.predict(features)
+
+    score = float(result[0])
+
+    if score < 0.3:
+        category = "Low Risk"
+    elif score < 0.6:
+        category = "Medium Risk"
+    else:
+        category = "High Risk"
+
+    return {
+        "risk_score": score,
+        "risk_category": category
+    }
